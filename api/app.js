98bfa480a385992acd12829eb85f534f6c32ac6e
dwnppoalt <dwnppoalt@outlook.com>
2025-05-04 21:43:33 +0800

    allow origin to all

--- api/app.js
+++ api/app.js
@@ -2,20 +2,8 @@ import express from 'express';
 import messageRoutes from '../src/routes/message.routes.js';
 const cors = require('cors');
 
-const allowedOrigins = [
-  'http://localhost:5173',   // Local development
-  'https://nocturne-black.vercel.app/'
-];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // Handle non-origin requests like from Postman
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+const corsOptions = {origin: '*',};
 
 app.use(cors(corsOptions));
 
